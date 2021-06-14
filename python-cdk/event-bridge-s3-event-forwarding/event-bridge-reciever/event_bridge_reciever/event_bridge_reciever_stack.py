33e132dbe5a3448eaeb867b201ea3ef961495db5
Jeremy Tirrell <jtirrell@athabascau.ca>
2021-06-14 14:58:33 -0600

    Updated the rule to use a input transformer.

--- python-cdk/event-bridge-s3-event-forwarding/event-bridge-reciever/event_bridge_reciever/event_bridge_reciever_stack.py
+++ python-cdk/event-bridge-s3-event-forwarding/event-bridge-reciever/event_bridge_reciever/event_bridge_reciever_stack.py
@@ -1,6 +1,7 @@
 from logging import error
 import boto3
 import botocore
+import typing
 from botocore.exceptions import ClientError
 from aws_cdk import (
     aws_cloudtrail,
@@ -31,6 +32,7 @@ class EventBridgeRecieverStack(core.Stack):
         validate_cdk_json(self)
         naming_prefix = self.node.try_get_context("naming_prefix")
         aws_organization = boto3.client('organizations')
+        lambda_function_arn = self.node.try_get_context("lambda_function_arn")
         try:
             response = aws_organization.describe_organization()
             orgId = response['Organization']['Id']
@@ -73,5 +75,38 @@ class EventBridgeRecieverStack(core.Stack):
                        'key': 'aws:PrincipalOrgID', 'value': orgId},
             principal="*",
             event_bus_name=event_bus.event_bus_name
-            # statement=f"{bus_statement}"
+        )
+
+        ######################################################################
+        # create event rule
+        ######################################################################
+
+        # Setup Mapping for the input tranformer
+        input_paths_map = {}
+        input_paths_map['bucketName'] = '$.detail.requestParameters.bucketName'
+        input_paths_map['key'] = '$.detail.requestParameters.key'
+        # Define the input transformer
+        input_transformer_property = aws_events.CfnRule.InputTransformerProperty(
+            input_template='{\"Records\": [{\n \"s3\": {\n \"bucket\": {\n \"name\": \"<bucketName>\"\n },\n \"object\": {\n \"key\": \"<key>\"}}}]}',
+            input_paths_map=input_paths_map
+        )
+        # Create Lambda target
+        lambda_target = aws_events.CfnRule.TargetProperty(
+            arn=lambda_function_arn,
+            id=f"{naming_prefix}aes-loader-trigger",
+            input_transformer=input_transformer_property
+        )
+        # Put target property into a list
+        target_list = []
+        target_list.append(lambda_target)
+        # Create the rule
+        cfn_rule = aws_events.CfnRule(
+            self,
+            id=f"{naming_prefix}aes-event-rule",
+            event_bus_name=event_bus.event_bus_arn,
+            event_pattern={
+                "eventSource": ["s3.amazonaws.com"],
+                "eventName": ["putObject"]
+            },
+            targets=target_list
         )
