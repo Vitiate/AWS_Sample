8b561c487c81b4519a25168c0c4b53d07c1678a1
Jeremy Tirrell <jeremy@tirrell.ca>
2021-08-20 14:43:21 -0600

    testing

--- python/war-automation/war.py
+++ python/war-automation/war.py
@@ -6,7 +6,7 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
 # https://aws.amazon.com/apache2.0/
-
+ 
 
 import botocore
 import boto3
